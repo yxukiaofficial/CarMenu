025e9f54a131ea62cca827eb96c7994d8f4a2f0e
Yxukia <137515645+yxukiaofficial@users.noreply.github.com>
2024-05-31 22:54:28 -0700

    Add files via upload

---
+++ CarMenu/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'yxukiaCarMenu'
+version '1.0.0'
+description 'FiveM car menu with multiple features including mass spawn prevention, and a sleek look.'
+author 'Yxukia'
+
+client_scripts {
+    '@menuv/menuv.lua',
+    'menu.lua'
+}
+
+dependencies {
+    'menuv'
+}
